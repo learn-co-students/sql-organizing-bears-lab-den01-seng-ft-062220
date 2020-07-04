4639895eae5d6f301bee7729fab0a8b55207ba21
NyaradzoUBere <nybe5598@colorado.edu>
2020-07-04 15:27:16 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,33 @@
+INSERT INTO bears 
+(name, age, gender, color, temperament, alive)
+VALUES ('Mr.  Chocolate', 21, 'M', 'Brown', 'Mild', true);
+
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES ('Rowdy', 34, 'M', 'Black', 'Goofy', false);
+
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 14, 'F', 'Black', 'Low', true);
+
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES ('Sergeant Brown', 22, 'F', 'Brown', 'High', true);
+
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 28, 'F', 'Brown', 'Mild', true);
+
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES ('Grinch', 25, 'M', 'Brown', 'High', false);
+
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES ('Wendy', 10, 'F', 'Brown', 'Mild', true);
+
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES (NULL, 30, 'M', 'Black', 'High', true);
+
+
